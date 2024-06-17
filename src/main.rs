cd2fb504e5926afb495df39daf65dfaa64070af2
holzmanj <jesseholzman96@gmail.com>
2024-06-17 09:26:53 -0400

    Open audio file and add buttons to play/stop

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
-use eframe::egui;
+use eframe::egui::{self, Color32};
+use rodio::buffer::SamplesBuffer;
+use rodio::source::Buffered;
+use rodio::{source::Source, Decoder, OutputStream, OutputStreamHandle, Sink};
+use std::fs::File;
+use std::io::BufReader;
+
+type AudioSource = Buffered<Decoder<BufReader<File>>>;
+
+struct AudioFile {
+    name: String,
+    source: AudioSource,
+}
+
+struct NebulizerApp {
+    stream_handle: OutputStreamHandle,
+    sink: Sink,
+
+    audio_file: Option<AudioFile>,
+}
+
+impl NebulizerApp {
+    fn new(stream_handle: OutputStreamHandle, sink: Sink) -> NebulizerApp {
+        NebulizerApp {
+            stream_handle,
+            sink,
+
+            audio_file: None,
+        }
+    }
+}
 
 fn main() {
+    // initialize audio output stream
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
     let native_options = eframe::NativeOptions::default();
     let _ = eframe::run_native(
         "nebulizer",
         native_options,
-        Box::new(|_cc| Box::new(NebulizerApp::default())),
+        Box::new(|_cc| Box::new(NebulizerApp::new(stream_handle, sink))),
     );
-}
-
-#[derive(Default)]
-struct NebulizerApp {
-    dropped_file: Option<egui::DroppedFile>,
 }
 
 impl eframe::App for NebulizerApp {
@@ -19,28 +48,51 @@ impl eframe::App for NebulizerApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("[UNDER CONSTRUCTION]");
 
-            match &self.dropped_file {
+            if ui.button("Open file").clicked() {
+                if let Some(path) = rfd::FileDialog::new().pick_file() {
+                    // attempt to load and decode audio file
+                    if let Some(source) = load_audio_file(path.display().to_string()) {
+                        self.audio_file = Some(AudioFile {
+                            name: path.display().to_string(),
+                            source,
+                        })
+                    } else {
+                        ui.colored_label(Color32::RED, "Failed to read/decode audio file!");
+                    }
+                }
+            }
+
+            ui.label("File:");
+            match &self.audio_file {
                 None => {
-                    ui.label("Drag a file onto the window :)");
+                    ui.monospace("[none]");
                 }
                 Some(file) => {
-                    let info = if let Some(path) = &file.path {
-                        path.display().to_string()
-                    } else {
-                        "???".to_owned()
-                    };
+                    ui.monospace(&file.name);
+                    if ui.button("Play").clicked() {
+                        self.sink.stop();
 
-                    ui.label("File:");
-                    ui.monospace(info);
+                        let source = file.source.clone();
+                        self.sink.append(source);
+                    }
+
+                    if ui.button("Stop").clicked() {
+                        self.sink.stop();
+                    }
                 }
             }
         });
+    }
+}
 
-        // collect dropped file
-        ctx.input(|i| {
-            if !i.raw.dropped_files.is_empty() {
-                self.dropped_file = i.raw.dropped_files.first().cloned();
-            }
-        });
+fn load_audio_file(path: String) -> Option<AudioSource> {
+    if let Some(file) = File::open(path).ok() {
+        if let Some(decoder) = Decoder::new(BufReader::new(file)).ok() {
+            Some(decoder.buffered())
+        } else {
+            None
+        }
+    } else {
+        None
     }
 }
