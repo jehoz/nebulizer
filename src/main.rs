cfe65303ac0b8f61edd5519526eb909915821aa5
holzmanj <jesseholzman96@gmail.com>
2024-06-18 08:10:40 -0400

    Play audio in separate thread

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use eframe::egui::{self, Color32};
-use rodio::buffer::SamplesBuffer;
 use rodio::source::Buffered;
 use rodio::{source::Source, Decoder, OutputStream, OutputStreamHandle, Sink};
 use std::fs::File;
 use std::io::BufReader;
+use std::sync::{Arc, Mutex};
+use std::thread;
 use std::time::Duration;
 
 type AudioSource = Buffered<Decoder<BufReader<File>>>;
@@ -14,7 +15,7 @@ struct AudioFile {
     source: AudioSource,
 }
 
-struct NebulizerApp {
+struct AppState {
     stream_handle: OutputStreamHandle,
     sink: Sink,
 
@@ -25,9 +26,9 @@ struct NebulizerApp {
     grain_size: u32,
 }
 
-impl NebulizerApp {
-    fn new(stream_handle: OutputStreamHandle, sink: Sink) -> NebulizerApp {
-        NebulizerApp {
+impl AppState {
+    fn new(stream_handle: OutputStreamHandle, sink: Sink) -> AppState {
+        AppState {
             stream_handle,
             sink,
 
@@ -40,29 +41,59 @@ impl NebulizerApp {
     }
 }
 
+struct NebulizerApp {
+    state: Arc<Mutex<AppState>>,
+}
+
+fn play_audio(state: Arc<Mutex<AppState>>) {
+    loop {
+        let state = state.lock().unwrap();
+
+        if let Some(file) = &state.audio_file {
+            if state.playing && state.sink.empty() {
+                let start_time: Duration = Duration::from_millis(
+                    ((state.grain_start_position * file.size as f32)
+                        / file.source.sample_rate() as f32) as u64,
+                );
+
+                let grain = file
+                    .source
+                    .clone()
+                    .skip_duration(start_time)
+                    .take_duration(Duration::from_millis(state.grain_size as u64));
+                state.sink.append(grain);
+            }
+        }
+    }
+}
+
 fn main() {
     // initialize audio output stream
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
 
+    let state = Arc::new(Mutex::new(AppState::new(stream_handle, sink)));
+    let app = NebulizerApp {
+        state: state.clone(),
+    };
+
+    thread::spawn(move || play_audio(state));
+
     let native_options = eframe::NativeOptions::default();
-    let _ = eframe::run_native(
-        "nebulizer",
-        native_options,
-        Box::new(|_cc| Box::new(NebulizerApp::new(stream_handle, sink))),
-    );
+    let _ = eframe::run_native("nebulizer", native_options, Box::new(|_cc| Box::new(app)));
 }
 
 impl eframe::App for NebulizerApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            let mut state = self.state.lock().unwrap();
             ui.heading("[UNDER CONSTRUCTION]");
 
             if ui.button("Open file").clicked() {
                 if let Some(path) = rfd::FileDialog::new().pick_file() {
                     // attempt to load and decode audio file
                     if let Some(source) = load_audio_file(path.display().to_string()) {
-                        self.audio_file = Some(AudioFile {
+                        state.audio_file = Some(AudioFile {
                             name: path.display().to_string(),
                             size: source.clone().count(),
                             source,
@@ -74,7 +105,7 @@ impl eframe::App for NebulizerApp {
             }
 
             ui.label("File:");
-            match &self.audio_file {
+            match &state.audio_file {
                 None => {
                     ui.monospace("[none]");
                 }
@@ -82,46 +113,27 @@ impl eframe::App for NebulizerApp {
                     ui.monospace(&file.name);
                     ui.horizontal(|ui| {
                         if ui.button("Play").clicked() {
-                            self.playing = true;
+                            state.playing = true;
                         }
 
                         if ui.button("Stop").clicked() {
-                            self.sink.stop();
-                            self.playing = false;
+                            state.sink.stop();
+                            state.playing = false;
                         }
                     });
 
                     ui.horizontal(|ui| {
                         ui.label("Start position");
                         ui.add(
-                            egui::Slider::new(&mut self.grain_start_position, 0.0..=100.0)
+                            egui::Slider::new(&mut state.grain_start_position, 0.0..=100.0)
                                 .suffix("%"),
                         );
                     });
 
                     ui.horizontal(|ui| {
                         ui.label("Grain size");
-                        ui.add(egui::Slider::new(&mut self.grain_size, 10..=100).suffix("ms"));
+                        ui.add(egui::Slider::new(&mut state.grain_size, 10..=100).suffix("ms"));
                     });
-                }
-            }
-
-            if self.playing {
-                if let Some(file) = &self.audio_file {
-                    if self.sink.empty() {
-                        let start_time: Duration = Duration::from_millis(
-                            ((self.grain_start_position * file.size as f32)
-                                / file.source.sample_rate() as f32)
-                                as u64,
-                        );
-
-                        let grain = file
-                            .source
-                            .clone()
-                            .skip_duration(start_time)
-                            .take_duration(Duration::from_millis(self.grain_size as u64));
-                        self.sink.append(grain);
-                    }
                 }
             }
         });
