0bd8c740ef3a0a95a2d0b1e3ccbe3ce6fb353f4e
holzmanj <jesseholzman96@gmail.com>
2024-06-19 21:21:17 -0400

    Break emitter settings out into type, share in mutex

--- src/emitter.rs
+++ src/emitter.rs
@@ -3,14 +3,31 @@ use std::time::Duration;
 
 use crate::grain::Grain;
 
+#[derive(Clone)]
+pub struct EmitterSettings {
+    pub position: f32,
+    pub grain_size_ms: f32,
+    pub envelope: f32,
+    pub overlap: f32,
+}
+
+impl Default for EmitterSettings {
+    fn default() -> Self {
+        EmitterSettings {
+            position: 0.0,
+            grain_size_ms: 25.0,
+            envelope: 0.5,
+            overlap: 0.0,
+        }
+    }
+}
+
 pub struct Emitter<I> {
     input: I,
 
-    position: f32,
-    grain_size: Duration,
-    envelope: f32,
-    overlap: f32,
+    pub settings: EmitterSettings,
 
+    ms_since_last_grain: f32,
     grains: Vec<Grain<I>>,
 }
 
@@ -19,52 +36,75 @@ where
     I: Clone + Source,
     I::Item: Sample,
 {
-    fn new(input: I) -> Emitter<I>
+    pub fn new(input: I) -> Emitter<I>
     where
-        I: Source,
+        I: Clone + Source,
         I::Item: Sample,
     {
         Emitter {
             input,
+            settings: EmitterSettings::default(),
 
-            position: 0.0,
-            grain_size: Duration::from_millis(25),
-            envelope: 0.5,
-            overlap: 0.0,
-
+            ms_since_last_grain: 0.0,
             grains: Vec::new(),
         }
+    }
+
+    pub fn make_grain(&self) -> Grain<I> {
+        let start = self
+            .input
+            .total_duration()
+            .unwrap()
+            .mul_f32(self.settings.position);
+        let size = Duration::from_nanos((1000000.0 * self.settings.grain_size_ms) as u64);
+        Grain::new(&self.input, start, size, self.settings.envelope)
+    }
+
+    fn grain_interval_ms(&self) -> f32 {
+        self.settings.grain_size_ms * (1.0 - self.settings.overlap)
     }
 }
 
 impl<I> Iterator for Emitter<I>
 where
     I: Clone + Source,
-    I::Item: Sample,
+    I::Item: Default + Sample,
 {
     type Item = <I as Iterator>::Item;
 
     #[inline]
     fn next(&mut self) -> Option<Self::Item> {
         // filter out grains that are done playing
-        self.grains = self
-            .grains
-            .into_iter()
-            .filter(|g| !g.done_playing())
-            .collect();
+        self.grains.retain(|g| !g.done_playing());
+
+        // make new grain if needed
+        self.ms_since_last_grain += 1000.0 / (self.sample_rate() as f32 * self.channels() as f32);
+        if self.ms_since_last_grain >= self.grain_interval_ms() {
+            let g = self.make_grain();
+            self.grains.push(g);
+            self.ms_since_last_grain = 0.0;
+        }
 
         // mix all grain samples into one
-        self.grains
-            .into_iter()
-            .filter_map(|mut g| g.next())
-            .reduce(|a, b| a.saturating_add(b))
+        let mut samples: Vec<I::Item> = Vec::new();
+        for grain in self.grains.iter_mut() {
+            if let Some(sample) = grain.next() {
+                samples.push(sample);
+            }
+        }
+
+        if let Some(sample) = samples.into_iter().reduce(|a, b| a.saturating_add(b)) {
+            Some(sample)
+        } else {
+            Some(I::Item::default())
+        }
     }
 }
 
 impl<I> Source for Emitter<I>
 where
     I: Clone + Iterator + Source,
-    I::Item: Sample,
+    I::Item: Default + Sample,
 {
     fn current_frame_len(&self) -> Option<usize> {
         None
--- src/main.rs
+++ src/main.rs
@@ -3,68 +3,22 @@ use rodio::source::Buffered;
 use rodio::{source::Source, Decoder, OutputStream, OutputStreamHandle, Sink};
 use std::fs::File;
 use std::io::BufReader;
+use std::ops::Deref;
 use std::sync::{Arc, Mutex};
-use std::thread;
 use std::time::Duration;
 
-type AudioSource = Buffered<Decoder<BufReader<File>>>;
+mod emitter;
+mod grain;
 
-struct AudioFile {
-    name: String,
-    size: usize,
-    source: AudioSource,
-}
+use emitter::{Emitter, EmitterSettings};
 
-struct AppState {
+type AudioClip = Buffered<Decoder<BufReader<File>>>;
+
+struct NebulizerApp {
     stream_handle: OutputStreamHandle,
     sink: Sink,
 
-    audio_file: Option<AudioFile>,
-    playing: bool,
-
-    grain_start_position: f32,
-    grain_size: u32,
-}
-
-impl AppState {
-    fn new(stream_handle: OutputStreamHandle, sink: Sink) -> AppState {
-        AppState {
-            stream_handle,
-            sink,
-
-            audio_file: None,
-            playing: false,
-
-            grain_start_position: 0.0,
-            grain_size: 20,
-        }
-    }
-}
-
-struct NebulizerApp {
-    state: Arc<Mutex<AppState>>,
-}
-
-fn play_audio(state: Arc<Mutex<AppState>>) {
-    loop {
-        let state = state.lock().unwrap();
-
-        if let Some(file) = &state.audio_file {
-            if state.playing && state.sink.empty() {
-                let start_time: Duration = Duration::from_millis(
-                    ((state.grain_start_position * file.size as f32)
-                        / file.source.sample_rate() as f32) as u64,
-                );
-
-                let grain = file
-                    .source
-                    .clone()
-                    .skip_duration(start_time)
-                    .take_duration(Duration::from_millis(state.grain_size as u64));
-                state.sink.append(grain);
-            }
-        }
-    }
+    emitter_settings: Arc<Mutex<EmitterSettings>>,
 }
 
 fn main() {
@@ -72,12 +26,12 @@ fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
 
-    let state = Arc::new(Mutex::new(AppState::new(stream_handle, sink)));
+    let settings = Arc::new(Mutex::new(EmitterSettings::default()));
     let app = NebulizerApp {
-        state: state.clone(),
+        stream_handle,
+        sink,
+        emitter_settings: settings,
     };
-
-    thread::spawn(move || play_audio(state));
 
     let native_options = eframe::NativeOptions::default();
     let _ = eframe::run_native("nebulizer", native_options, Box::new(|_cc| Box::new(app)));
@@ -86,61 +40,45 @@ fn main() {
 impl eframe::App for NebulizerApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            let mut state = self.state.lock().unwrap();
             ui.heading("[UNDER CONSTRUCTION]");
 
             if ui.button("Open file").clicked() {
                 if let Some(path) = rfd::FileDialog::new().pick_file() {
                     // attempt to load and decode audio file
                     if let Some(source) = load_audio_file(path.display().to_string()) {
-                        state.audio_file = Some(AudioFile {
-                            name: path.display().to_string(),
-                            size: source.clone().count(),
-                            source,
-                        })
+                        let settings_copy = self.emitter_settings.clone();
+                        let emitter = Emitter::new(source).periodic_access(
+                            Duration::from_millis(5),
+                            move |e| {
+                                let settings = settings_copy.lock().unwrap();
+                                e.settings = settings.deref().clone();
+                            },
+                        );
+                        self.sink.clear();
+                        self.sink.append(emitter);
+                        self.sink.play();
                     } else {
                         ui.colored_label(Color32::RED, "Failed to read/decode audio file!");
                     }
                 }
             }
 
-            ui.label("File:");
-            match &state.audio_file {
-                None => {
-                    ui.monospace("[none]");
-                }
-                Some(file) => {
-                    ui.monospace(&file.name);
-                    ui.horizontal(|ui| {
-                        if ui.button("Play").clicked() {
-                            state.playing = true;
-                        }
+            let mut settings = self.emitter_settings.lock().unwrap();
+            ui.monospace("Emitter settings");
+            ui.horizontal(|ui| {
+                ui.label("Position");
+                ui.add(egui::Slider::new(&mut settings.position, 0.0..=1.0));
+            });
 
-                        if ui.button("Stop").clicked() {
-                            state.sink.stop();
-                            state.playing = false;
-                        }
-                    });
-
-                    ui.horizontal(|ui| {
-                        ui.label("Start position");
-                        ui.add(
-                            egui::Slider::new(&mut state.grain_start_position, 0.0..=100.0)
-                                .suffix("%"),
-                        );
-                    });
-
-                    ui.horizontal(|ui| {
-                        ui.label("Grain size");
-                        ui.add(egui::Slider::new(&mut state.grain_size, 10..=100).suffix("ms"));
-                    });
-                }
-            }
+            ui.horizontal(|ui| {
+                ui.label("Grain size");
+                ui.add(egui::Slider::new(&mut settings.grain_size_ms, 1.0..=1000.0).suffix("ms"));
+            });
         });
     }
 }
 
-fn load_audio_file(path: String) -> Option<AudioSource> {
+fn load_audio_file(path: String) -> Option<AudioClip> {
     if let Some(file) = File::open(path).ok() {
         if let Some(decoder) = Decoder::new(BufReader::new(file)).ok() {
             Some(decoder.buffered())
