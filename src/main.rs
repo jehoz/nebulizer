1f7aff49d06ff516bb82bd0802dc4a0fa5a48f4a
holzmanj <jesseholzman96@gmail.com>
2024-06-23 10:21:40 -0400

    Use dark mode

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,22 @@ mod grain;
 mod midi;
 
 use app::NebulizerApp;
+use eframe::egui::{Style, Visuals};
 
 fn main() {
-    // match midi::run() {
-    //     Ok(_) => (),
-    //     Err(err) => println!("FUCK {}", err),
-    // }
-
     let app = NebulizerApp::new();
 
     let native_options = eframe::NativeOptions::default();
-    let _ = eframe::run_native("nebulizer", native_options, Box::new(|_cc| Box::new(app)));
+    let _ = eframe::run_native(
+        "nebulizer",
+        native_options,
+        Box::new(|cc| {
+            let style = Style {
+                visuals: Visuals::dark(),
+                ..Style::default()
+            };
+            cc.egui_ctx.set_style(style);
+            Box::new(app)
+        }),
+    );
 }
