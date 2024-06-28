573ee4a49ad24d3e82961af22e59917fdd941bcf
holzmanj <jesseholzman96@gmail.com>
2024-06-28 09:38:06 -0400

    Tweak some envelope plot stuff

--- src/widgets/envelope_plot.rs
+++ src/widgets/envelope_plot.rs
@@ -10,27 +10,25 @@ pub fn envelope_plot(ui: &mut Ui, amount: f32, skew: f32) {
     Frame::canvas(ui.style()).show(ui, |ui| {
         let color = Color32::from_white_alpha(240);
 
-        let desired_size = ui.available_width() * vec2(1.0, 0.35);
+        let desired_size = ui.available_width() * vec2(1.0, 0.5);
         let (_id, rect) = ui.allocate_space(desired_size);
 
         let to_screen =
-            emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, -1.0..=1.0), rect);
+            emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, 1.0..=-1.0), rect);
 
         let mut shapes = vec![];
-        {
-            let n = 120;
+        let n = 120;
 
-            let points: Vec<Pos2> = (0..=n)
-                .map(|i| {
-                    let t = i as f32 / (n as f32);
-                    let y = tukey_window(t, 1.0, amount, skew);
+        let points: Vec<Pos2> = (0..=n)
+            .map(|i| {
+                let x = i as f32 / (n as f32);
+                let y = tukey_window(x, 1.0, amount, skew);
 
-                    to_screen * pos2(t as f32, -1.0 * y + 0.5)
-                })
-                .collect();
+                to_screen * pos2(x, y - 0.5)
+            })
+            .collect();
 
-            shapes.push(epaint::Shape::line(points, Stroke::new(1.0, color)));
-        }
+        shapes.push(epaint::Shape::line(points, Stroke::new(1.0, color)));
 
         ui.painter().extend(shapes)
     });
