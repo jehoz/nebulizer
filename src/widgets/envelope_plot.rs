9d7de0b5d6f259d0bfbffa1e6e051f8fc95d3786
holzmanj <jesseholzman96@gmail.com>
2024-07-08 14:15:52 -0400

    Add envelope plot for adsr envelopes

--- src/widgets/envelope_plot.rs
+++ src/widgets/envelope_plot.rs
@@ -1,18 +1,39 @@
-use crate::envelope::GrainEnvelope;
+use crate::envelope::{AdsrEnvelope, GrainEnvelope};
 
 use eframe::{
-    egui::{pos2, vec2, Color32, Frame, Pos2, Rect, Ui, Widget},
+    egui::{pos2, vec2, Color32, Frame, Rect, Ui, Widget},
     emath,
     epaint::{self, Stroke},
 };
 
+enum Envelope<'a> {
+    Grain(&'a GrainEnvelope),
+    Adsr(&'a AdsrEnvelope),
+}
+
 pub struct EnvelopePlot<'a> {
-    envelope: &'a GrainEnvelope,
+    envelope: Envelope<'a>,
+    height: Option<f32>,
 }
 
 impl<'a> EnvelopePlot<'a> {
-    pub fn new(envelope: &'a GrainEnvelope) -> Self {
-        Self { envelope }
+    pub fn from_grain_envelope(envelope: &'a GrainEnvelope) -> Self {
+        Self {
+            envelope: Envelope::Grain(envelope),
+            height: None,
+        }
+    }
+
+    pub fn from_adsr_envelope(envelope: &'a AdsrEnvelope) -> Self {
+        Self {
+            envelope: Envelope::Adsr(envelope),
+            height: None,
+        }
+    }
+
+    pub fn set_height(mut self, height: f32) -> EnvelopePlot<'a> {
+        self.height = Some(height);
+        self
     }
 }
 
@@ -20,32 +41,58 @@ impl<'a> Widget for EnvelopePlot<'a> {
     fn ui(self, ui: &mut Ui) -> eframe::egui::Response {
         Frame::canvas(ui.style())
             .show(ui, |ui| {
-                let color = Color32::from_white_alpha(240);
-
-                let desired_size = ui.available_width() * vec2(1.0, 0.5);
+                let height = self.height.unwrap_or(1.0 * ui.available_width());
+                let desired_size = vec2(ui.available_width(), height);
                 let (_id, rect) = ui.allocate_space(desired_size);
 
-                let to_screen = emath::RectTransform::from_to(
-                    Rect::from_x_y_ranges(0.0..=1.0, 1.0..=-1.0),
-                    rect,
-                );
-
-                let mut shapes = vec![];
-                let n = 120;
-
-                let points: Vec<Pos2> = (0..=n)
-                    .map(|i| {
-                        let x = i as f32 / (n as f32);
-                        let y = self.envelope.amplitude_at(x);
-
-                        to_screen * pos2(x, y - 0.5)
-                    })
-                    .collect();
-
-                shapes.push(epaint::Shape::line(points, Stroke::new(1.0, color)));
-
-                ui.painter().extend(shapes)
+                match self.envelope {
+                    Envelope::Grain(env) => draw_grain_envelope(ui, env, rect),
+                    Envelope::Adsr(env) => draw_asdr_envelope(ui, env, rect),
+                }
             })
             .response
     }
+}
+
+fn draw_grain_envelope(ui: &mut Ui, envelope: &GrainEnvelope, rect: Rect) {
+    let color = Color32::from_white_alpha(240);
+    let to_screen =
+        emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, 1.0..=-1.0), rect);
+
+    let n = 120;
+    let points = (0..=n)
+        .map(|i| {
+            let x = i as f32 / (n as f32);
+            let y = envelope.amplitude_at(x);
+
+            to_screen * pos2(x, y - 0.5)
+        })
+        .collect();
+
+    let line = epaint::Shape::line(points, Stroke::new(1.0, color));
+    ui.painter().add(line);
+}
+
+fn draw_asdr_envelope(ui: &mut Ui, envelope: &AdsrEnvelope, rect: Rect) {
+    let color = Color32::from_white_alpha(240);
+    let to_screen =
+        emath::RectTransform::from_to(Rect::from_x_y_ranges(0.0..=1.0, 1.0..=0.0), rect);
+
+    let AdsrEnvelope {
+        attack_ms,
+        decay_ms,
+        sustain_level,
+        release_ms,
+    } = envelope;
+    let total_ms = attack_ms + decay_ms + release_ms;
+
+    let points = vec![
+        to_screen * pos2(0.0, 0.0),
+        to_screen * pos2(attack_ms / total_ms, 1.0),
+        to_screen * pos2((attack_ms + decay_ms) / total_ms, *sustain_level),
+        to_screen * pos2(1.0, 0.0),
+    ];
+
+    let line = epaint::Shape::line(points, Stroke::new(1.0, color));
+    ui.painter().add(line);
 }
