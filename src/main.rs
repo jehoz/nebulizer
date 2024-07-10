947b2aac3cb2410fa600f3e6da9ee8b488fbf4a0
holzmanj <jesseholzman96@gmail.com>
2024-07-09 21:52:44 -0400

    Use numeric module

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,18 @@ mod emitter;
 mod envelope;
 mod grain;
 mod midi;
+mod numeric;
 mod widgets;
 
 use app::NebulizerApp;
-use eframe::egui::{Style, Visuals};
+use eframe::egui::{Style, Vec2, Visuals};
 
 fn main() {
     let app = NebulizerApp::new();
 
-    let native_options = eframe::NativeOptions::default();
+    let mut native_options = eframe::NativeOptions::default();
+    native_options.viewport.inner_size = Some(Vec2 { x: 450.0, y: 600.0 });
+
     let _ = eframe::run_native(
         "nebulizer",
         native_options,
